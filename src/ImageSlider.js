687d5d6edc6a279575afc8a9af1ce14716da0683
louis baptiste <louisvis@ymail.com>
2020-06-27 22:02:06 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -12,7 +12,7 @@ class ImageSlider extends React.Component {
   render() {
     return (
       <div>
-          I am on slide  {this.state.currentSlideIndex}
+          I am on slide {this.state.currentSlideIndex}
       </div>
     );
   }
